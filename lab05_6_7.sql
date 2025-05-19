0095dfd5e196ba180b32197738646bb662ee0833
Minh Ha <minhha1192003@gmail.com>
2025-05-19 23:40:11 +0700

    cau 35 in ra ds sinh vien thi nhieu hon 4 mon

--- lab05_6_7.sql
+++ lab05_6_7.sql
@@ -345,3 +345,13 @@ select sv.makh,kh.tenkh,count(*) as tongsosvkh from SinhVien sv
 inner join khoa kh on sv.MaKH =kh.makh
 group by sv.MaKH,kh.tenkh
 having COUNT(*) >2
+-------cau 36------
+SELECT 
+    kq.masv, 
+    sv.makh, 
+    sv.hosv + ' ' + sv.tensv AS hoten,
+    COUNT(kq.MaMH) AS somonthi
+FROM Ketqua kq
+INNER JOIN SinhVien sv ON kq.masv = sv.masv
+GROUP BY kq.masv, sv.makh, sv.hosv, sv.tensv
+HAVING COUNT(kq.MaMH) > 4;
